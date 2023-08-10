5f10907da814e59cded20706fd0ccf86f4893f59
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-10 16:09:11 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * main - the main function
- * return: 0
+ * main - this is the main function
+ * return: always 0
+ */
+ 
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
