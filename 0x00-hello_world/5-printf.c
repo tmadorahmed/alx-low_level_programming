890ea30e9952b1b4cb81aed2010b8664cfce7efb
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-10 16:19:40 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - this is the main
+ * retutn: 0 when sucsse
+ */
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
