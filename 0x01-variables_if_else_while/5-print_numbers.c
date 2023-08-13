27d32a59b02a7f8a1f8b372cf41fdf7cfa9e8637
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-13 14:35:06 -0700

    number

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int count;
+
+	count = 0;
+	while (count < 10)
+	{
+		printf("%d", count);
+		count++;
+	}
+	printf("\n");
+	return (0);
+}
