09af25bf616db864eacb141fa0c8d2db1400aded
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-13 14:52:50 -0700

    alpha

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+int count;
+count = 0;
+while (count < 26)
+{
+	putchar(122 - count);
+	count++;
+}
+printf("\n");
+return (0);
+}
