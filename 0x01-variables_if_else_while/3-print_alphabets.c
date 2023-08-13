92bb9c14d78bf92cd649b39841bdc4f99183f3e1
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-13 13:12:52 -0700

    Aat0Zz

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	char cc;
+
+	c = 'a';
+	cc = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (cc <= 'Z')
+	{
+		putchar(cc);
+		c++;
+	}
+	putchar('\n';
+			return (0);
+}
