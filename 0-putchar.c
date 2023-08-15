2a9162673e1dffd18c877c6f02b554fc23332ec2
tmadorahmed <tmadorahmed88@gmail.com>
2023-08-15 13:41:02 -0700

    putchar

---
+++ 0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the alphabet in lowercas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("/_putchar");
+	printf("/n");
+	return (0);
+}
+
