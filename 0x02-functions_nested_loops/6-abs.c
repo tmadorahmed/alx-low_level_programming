4a282a9a640f4316c2850c028a82ae3492147f94
tmador ahmed <tmadorahmed88@gmail.com>
2023-08-19 13:25:37 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - function
+ * @a: The Input
+ *
+ * Return: n if  positive,
+ *         -n if negative,
+ *
+ */
+int _abs(int a)
+{
+if (a >= 0)
+return (a);
+else
+return (-a);
+}
