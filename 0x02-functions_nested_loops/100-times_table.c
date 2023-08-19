86a5708e98007b509303a5f62cdb91010d470f1f
tmador ahmed <tmadorahmed88@gmail.com>
2023-08-19 15:04:31 -0700

    time table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_times_table -uses _putchar to print char table
+ * return char in lower 100 time
+ * @n: input
+ */
+void print_times_table(int n)
+{
+	int x;
+	int l;
+	int p;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (x = 0; x <= n; ++x)
+		{
+			_putchar(48);
+			for (l = 1; l <= n; ++l)
+			{
+				_putchar(',');
+				_putchar(' ');
+				p = x * l;
+				if (p <= 9)
+					_putchar(' ');
+				if (p <= 99)
+					_putchar(' ');
+				if (p <= 100)
+				{
+					_putchar((p / 100) + 48);
+					_putchar((p / 10) % 10 + 48);
+				}
+				else if (p <= 99 && p >= 10)
+					_putchar((p / 10) + 48);
+				_putchar((p % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
