25485b9842d48ed89088335cd4f167e6bd0348ee
tmador ahmed <tmadorahmed88@gmail.com>
2023-08-19 15:32:46 -0700

    f

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,13 +9,17 @@
 int main(void)
 {
 	int i;
-	unsigned long f1 = 0, f2 = 1, sum;
+	unsigned long f1;
+	unsigned long f2;
+	unsigned long sum;
+
+	f1 = 0;
+	f2 = 1;
 
 	for (i = 0; i < 50; i++)
 	{
 		sum = f1 + f2;
 		printf("lu%", sum);
-
 		f1 = f2;
 		f2 = sum;
 
